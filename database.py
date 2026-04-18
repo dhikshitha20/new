f1e0dea4bebb2c7175cad8df635ec4d41fe17136
Dhikshidha <dhikshitha2005@gmail.com>
2026-04-19 02:14:43 +0530

    fixed all app imports

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-from app.core.config import settings
+from config import settings
 
 connect_args = {"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}
 
